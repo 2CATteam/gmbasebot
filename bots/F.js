76f65105ab565700aaa43bc8c9586ddbd1d07119
Daniel Royer <2CATteam@gmail.com>
2019-06-22 19:13:14 -0500

    Fixed all modules

--- bots/F.js
+++ bots/F.js
@@ -3,7 +3,7 @@ const regex = /^f$/i;
 exports.checkMessage = function(message)
 {
 	console.log(message.sender_type)
-	if (regex.test(message.text) && Math.random() < 0.9 && message.sender_type == "user")
+	if (regex.test(message.text) && Math.random() < 0.2 && message.sender_type == "user")
 	{
 		console.log("F this");
 		return "F";
