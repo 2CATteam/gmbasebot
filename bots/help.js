b075ef004449973e9ad4755b2a5a44ca7f03b222
Daniel Royer <2CATteam@gmail.com>
2019-08-10 21:40:05 -0500

    Refactoring help

--- bots/help.js
+++ bots/help.js
@@ -4,8 +4,8 @@ const sender = require('./sender.js').send
 exports.helpGener = class helpGener {
 	constructor(arr) {
 		this.helpString = "Here are all the commands I can respond to: \n\n"
-		for (var module in arr) {
-			this.helpString += module.helpString + "\n"
+		for (var i = 0; i < arr.length; i++) {
+			this.helpString += arr[i].helpString + "\n"
 		}
 		this.helpString += "\nHope that wasn't too much! Remember, you can always type /help to read this again! Have fun!"
 	}
