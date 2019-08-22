4ba02d9ff658c4254df84dbeaba942ee8fcd027d
Daniel Royer <2CATteam@gmail.com>
2019-08-21 19:51:33 -0500

    Trying to add translate

---
+++ bots/translate.js
@@ -0,0 +1,41 @@
+const sender = require('./sender.js').send;
+
+const translateRegex = /^\/translate\s"(.+)"\s(\S+)\s?(\S+)?/i;
+
+function translateMessage(message) {
+	if (translateRegex.test(message.text) && process.env.YANDEX_KEY) {
+        const matches = message.text.match(translateRegex)
+        const text = encodeURI(matches[1])
+        var fromFlag = 'en'
+        var toFlag = matches[2]
+		if (toFlag == 'en') {
+			fromFlag = 'la'
+		}
+        if (matches.length > 3) {
+            if (matches[3] != undefined) {
+                fromFlag = matches[2]
+                toFlag = matches[3]
+			}
+        }
+        const options = {
+            hostname: "translate.yandex.net",
+            path: `/api/v1.5/tr.json/translate?key=${process.env.YANDEX_KEY}&text=${text}&lang=${fromFlag + "-" + toFlag}`,
+            method: "POST"
+        }
+        console.log(options)
+        var req = https.request(options, (res) => {
+            res.on('data', (d) => {
+                try {
+                    const toSend = JSON.parse(d.toString()).text
+                    sender(toSend)
+                } catch (err) {
+                    sender("There was an issue translating that. Please try again but better this time.")
+                }
+            })
+        })
+        req.on('error', (e) => {
+            console.log(e)
+        })
+        req.end()
+	}
+}
