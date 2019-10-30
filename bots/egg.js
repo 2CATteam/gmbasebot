15e1364830673aef33e047ac0ada2d58bd7783d0
Daniel Royer <2CATteam@gmail.com>
2019-10-29 23:40:46 -0500

    EGGG

--- bots/egg.js
+++ bots/egg.js
@@ -6,7 +6,10 @@ exports.helpString = "/egg"
 
 exports.checkMessage = function(egggg) {
 	console.log(egggg.text)
-	if (!eggg.test(egggg.text)) { return; }
+	if (!eggg.test(egggg.text)) { 
+		console.log("Not a match");
+		return;
+	}
 	fotology("EGG", function (imageURLs) {
 		for (var i in imageURLs) {
 			console.log(imageURLs[i]);
